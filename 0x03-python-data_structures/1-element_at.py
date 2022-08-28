c4939583164e3a9f3c052138d7210ee2e313167c
Blakdothraki <blakdothraki@github.com>
2022-08-28 23:57:34 +0100

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    
-    return my_list[idx]
+def print_list_integer(my_list=[]):
+    for x in range(0, len(my_list)):
+        print("{:d}".format(my_list[x]))
+
